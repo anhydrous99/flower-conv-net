63550ff3d17c80a56b0d50a8495db35e100475b0
Armando Herrera <anhydrous99@gmail.com>
2019-04-21 20:34:35 -0500

    Added pixel mean subtraction and a missing relu layer

--- create_model.py
+++ create_model.py
@@ -20,6 +20,11 @@ def create_model(x_train, y_train, x_test, y_test, n_classes, batch_size, epochs
     x_train = 2 * x_train / 255 - 1
     x_test = 2 * x_test / 255 - 1
 
+    # Subtract pixel mean
+    mean = np.mean(x_train, axis=0)
+    x_train -= mean
+    x_test -= mean
+
     # Create Model
     model = Sequential()
     model.add(Conv2D(32,                           # Number of learnable filters
@@ -28,7 +33,7 @@ def create_model(x_train, y_train, x_test, y_test, n_classes, batch_size, epochs
                      activation='relu',            # Kind of activation layer
                      input_shape=(25, 25, 3)))     # Shape of input, channels last
     model.add(MaxPooling2D((2, 2)))
-    model.add(Conv2D(64, (3, 3)))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(MaxPooling2D((2, 2)))
     model.add(Flatten())
     model.add(Dense(512, activation='relu'))
